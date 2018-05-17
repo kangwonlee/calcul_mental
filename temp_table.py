e820b2e3bc27e6ff811dc555a8c6ffa420e6e752
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-17 19:36:02 +0900

    Added present_get_min()
    Present a small table of doing_great vs min_addend

--- temp_table.py
+++ temp_table.py
@@ -1,3 +1,6 @@
+import calcul_mental
+
+
 def main():
     for row in range(10):
         row_list =['%2d %2d' % (row, row + col) for col in range(10)]
@@ -5,5 +8,11 @@ def main():
         print(row_string)
 
 
+def present_get_min():
+    for doing_great in range(30):
+
+        print('%2d %2d' % (doing_great, calcul_mental.get_min(doing_great)))
+
+
 if '__main__' == __name__:
-    main()
+    present_get_min()
