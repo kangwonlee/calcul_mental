50909435a582b85382ad580a687b97f20db573a7
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-05-17 19:57:30 +0900

    Added test_reset_all_level_params()
    Make abs(d) smaller
    Minimum <-- 1
    Maximum <-- 9

--- tests/test_calcul_mental.py
+++ tests/test_calcul_mental.py
@@ -3,9 +3,25 @@ import calcul_mental
 
 
 class testCalculMental(unittest.TestCase):
+
     def test_reset_doing_great(self):
         for d in range(1, 100):
             self.assertGreater(abs(d), abs(calcul_mental.reset_doing_great(d)))
 
         for d in range(-100, 0):
             self.assertGreater(abs(d), abs(calcul_mental.reset_doing_great(d)))
+
+    def test_reset_all_level_params(self):
+        for n in range(-10, 20 + 1):
+            for x in range(-10, 20 + 1):
+                for d in range(1, 100):
+                    new_d, new_x, new_n = calcul_mental.reset_all_level_params(d, n, x)
+                    self.assertGreater(abs(d), abs(new_d))
+                    self.assertEqual(1, new_n)
+                    self.assertEqual(9, new_x)
+
+                for d in range(-100, 0):
+                    new_d, new_x, new_n = calcul_mental.reset_all_level_params(d, n, x)
+                    self.assertGreater(abs(d), abs(new_d))
+                    self.assertEqual(1, new_n)
+                    self.assertEqual(9, new_x)
