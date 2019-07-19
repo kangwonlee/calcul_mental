1a8bf58fd346ed3709f22abe7f09547658e3c432
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-07-20 01:59:31 +0900

    refactored is_ready()

--- easier_mental.py
+++ easier_mental.py
@@ -3,7 +3,38 @@ import time
 
 
 def main():
-    pass
+    # to keep records
+    history = []
+
+    # for variable challange level
+    n_numbers = 2
+    min_number = 2
+    max_number = 9
+
+    # keep track of student's achievement
+    doing_great = 0
+
+    while is_ready():
+        pass
+
+
+def is_ready():
+    result = False
+
+    while True:
+
+        ready = input('Ready? ').strip().lower()
+
+        if ready.startswith('n'):
+            result = False
+            break
+        elif ready.startswith('y'):
+            result = True
+            break
+        else:
+            print('yes or no please?')
+
+    return result
 
 
 if "__main__" == __name__:
