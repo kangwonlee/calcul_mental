73eabd31c340460fcd630dccac5d0001551e124d
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-07-30 20:58:15 +0900

    started test operation class

---
+++ tests/test_operation_class.py
@@ -0,0 +1,15 @@
+import unittest
+
+import easier_mental
+
+
+class TestOperationBase(unittest.TestCase):
+
+    def setUp(self):
+        self.ob = easier_mental.OperationBase()
+
+    def tearDown(self):
+        del self.ob
+
+    def test_init(self):
+        self.assertIsInstance(self.ob.pick_list, (list, tuple, set))
