4af0ef9a62de86e595de53395f45d16bb41547d4
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-07-30 21:02:03 +0900

    check any number-to-be-excluded included

--- tests/test_operation_class.py
+++ tests/test_operation_class.py
@@ -13,3 +13,6 @@ class TestOperationBase(unittest.TestCase):
 
     def test_init(self):
         self.assertIsInstance(self.ob.pick_list, (list, tuple, set))
+
+        for except_this in self.ob.except_these:
+            self.assertNotIn(except_this, self.ob.pick_list)
